fab4128c89eb27e778b963472e984e4b60f7b3b6
Pedro-Novais <phnovais7@gmail.com>
2024-09-28 11:30:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const calculateTimeLeft = () => {
+    const difference = +new Date(`2025-01-01T00:00:00`) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        dias: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        horas: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutos: Math.floor((difference / 1000 / 60) % 60),
+        segundos: Math.floor((difference / 1000) % 60)
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    setMousePosition({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span key={interval}>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div className="App">
+      <div className="timer">
+        {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+      </div>
+
+      <div
+        className="blur"
+        style={{
+          left: `${mousePosition.x}px`,
+          top: `${mousePosition.y}px`,
+        }}
+      ></div>
+    </div>
+    
+  );
+}
+
+export default App;
